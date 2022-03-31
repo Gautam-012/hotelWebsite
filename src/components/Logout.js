be6db76f86033ca829db0208fa6e911c34ecaa46
Gautam Kumar <79142679+Gautam-012@users.noreply.github.com>
2022-03-31 10:57:41 +0530

    Add files via upload

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,7 @@
 import {GoogleLogout} from 'react-google-login';
 import { useAuth } from './hooks/useAuth';
 
-const clientId= "459598229133-6repc7crj75rl29cmnvu5dmdepq70qbj.apps.googleusercontent.com";
+const clientId= "897015532740-ab3t4eqqol8d5uab7iud3q1c4s55g7p9.apps.googleusercontent.com";
 
 const Logout =()=>{
     
