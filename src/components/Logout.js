e222b76672e2f0d1b5a5f9bfbd696318e4b0122c
Gautam Kumar <79142679+Gautam-012@users.noreply.github.com>
2022-04-28 00:40:32 +0530

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,6 @@
 import {GoogleLogout} from 'react-google-login';
 import { useAuth } from './hooks/useAuth';
 
-const clientId= "897015532740-ab3t4eqqol8d5uab7iud3q1c4s55g7p9.apps.googleusercontent.com";
 
 const Logout =()=>{
     
